5f7637c48cf8ef5c74e886ad1a54dcdcac666d15
mkstas <makarov.stn@gmail.com>
2024-05-13 21:05:17 +0300

    feat: update schema

--- prisma/migrations/20240513170817_initial/migration.sql
+++ prisma/migrations/20240513170817_initial/migration.sql
@@ -16,22 +16,22 @@ CREATE TABLE "clients" (
 );
 
 -- CreateTable
-CREATE TABLE "products" (
+CREATE TABLE "goods" (
     "id" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
-    "image_url" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
     "price" INTEGER NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "goods_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "orders" (
     "id" SERIAL NOT NULL,
-    "clientId" INTEGER NOT NULL,
-    "productId" INTEGER NOT NULL,
+    "client_id" INTEGER NOT NULL,
+    "good_id" INTEGER NOT NULL,
     "status" "OrderStatus" NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
@@ -40,13 +40,13 @@ CREATE TABLE "orders" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "orders_clientId_key" ON "orders"("clientId");
+CREATE UNIQUE INDEX "orders_client_id_key" ON "orders"("client_id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "orders_productId_key" ON "orders"("productId");
+CREATE UNIQUE INDEX "orders_good_id_key" ON "orders"("good_id");
 
 -- AddForeignKey
-ALTER TABLE "orders" ADD CONSTRAINT "orders_clientId_fkey" FOREIGN KEY ("clientId") REFERENCES "clients"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "orders" ADD CONSTRAINT "orders_client_id_fkey" FOREIGN KEY ("client_id") REFERENCES "clients"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "orders" ADD CONSTRAINT "orders_productId_fkey" FOREIGN KEY ("productId") REFERENCES "products"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "orders" ADD CONSTRAINT "orders_good_id_fkey" FOREIGN KEY ("good_id") REFERENCES "goods"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
